578a6715983ba65d7b6dd95899f139ad389919ce
Raul Garcia <rauljgarcia97@gmail.com>
2025-10-06 09:57:49 -0700

    Add chymotrypsin_cleave function and update markdown documentation

--- src/enzymes.py
+++ src/enzymes.py
@@ -1,55 +1,58 @@
-'''
+"""
 enzymes.py
 
 Contains functions for peptide cleavage by proteases.
 Currently supports trypsin cleavage.
-'''
+"""
 
-def cleave(seq, *, cut_after, block_if_next=None, case="upper"):
-    '''
+
+def cleave(seq, *, cut_after, block_if_next=None):
+    """
     This function split a peptide sequence into fragments based on cleavage rules
 
     Parameters:
     seq (str): Amino acid sequence
     cut after (set[str]): Residues where cleavage occurs on the C-terminal side
-    block_if_next (set[str]): Residues that block cleavage if they appear 
+    block_if_next (set[str]): Residues that block cleavage if they appear
         immediately after a cut site
-    
+
     Returns:
     list of strings, list of peptide fragments in order
-    '''
-    pass
+    """
+    cut_after = {str(x).upper() for x in cut_after}
+    block_if_next = {str(x).upper() for x in block_if_next} if block_if_next else None
 
-def trypsin_cleave(ply_pep):
-    '''
-    Cleaves a peptide sequence according to trypsin rules.
-
-        Trypsin cleaves on the C-terminal side of 'K' (Lysine) or 'R' (Arginine),
-        except when followed by 'P' (Proline).
-
-        Parameters:
-        ply_pep (str):
-            Input peptide sequence (string of one-letter amino acid codes).
-
-        Returns:
-        list of str
-            Peptide fragments produced by trypsin cleavage.
-    '''
-    ply_pep = ply_pep.upper()
-    segs = []
+    segments = []
     start = 0
-    for i in range(len(ply_pep)):
-        if (
-            ply_pep[i] in {"R", "K"} 
-            and (i+1 == len(ply_pep) or ply_pep[i+1] != "P")
-        ):
-            segs.append(ply_pep[start:i+1])
-            start = i+1
-    if start < len(ply_pep):
-        segs.append(ply_pep[start:])
-    return segs
+
+    for i in range(len(seq)):
+        is_cut_residue = seq[i] in cut_after
+        is_blocked = (
+            (i + 1 < len(seq)) and block_if_next and (seq[i + 1] in block_if_next)
+        )
+        if is_cut_residue and not is_blocked:
+            segments.append(seq[start : i + 1])
+            start = i + 1
+    if start < len(seq):
+        segments.append(seq[start:])
+    return segments
+
+
+def trypsin_cleave(seq):
+    """Cleave peptide after K or R, unless followed by P."""
+    return cleave(seq, cut_after={"R", "K"}, block_if_next={"P"})
+
+
+def chymotrypsin_cleave(seq):
+    """Cleave peptide after Y, W, F, L, or M, unless followed by P."""
+    return cleave(seq, cut_after={"Y", "W", "F", "L", "M"}, block_if_next={"P"})
+
 
 def main():
+
     print(trypsin_cleave("KMKAFLRGYV"))
+    print(chymotrypsin_cleave("AFYWLPGA"))
+
+
 if __name__ == "__main__":
     main()
